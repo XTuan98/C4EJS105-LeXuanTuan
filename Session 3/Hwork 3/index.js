af0524fdf2225306c200c6d104c17e5b41e759f8
XTuan98 <lexuantuan.k66@gmail.com>
2020-12-24 04:40:25 +0700

    add homework 3

---
+++ Session 3/Hwork 3/index.js
@@ -0,0 +1,32 @@
+console.log('Ex1');
+////   Using Destructuring assignment
+// let a = 5;
+// let b = 6;
+// [a , b] = [b, a];
+// console.log(a,b);
+////   Using Temporary
+// let a = 4;
+// let b = 9;
+// let Temporary;
+// Temporary = a;
+// a = b;
+// b = Temporary;
+// console.log(a,b);
+
+// console.log('Ex2');
+// const s = 'Hello beauty there'
+// const a = s.split(" ");
+// console.log(a)
+
+
+// console.log('Ex3');
+
+// const a = [4, 5, 7, -8];
+// console.log(...a);
+
+// const a = ['banh','trưng','ran'];
+// console.log(...a)
+
+console.log('Ex4');
+
+// Let userNhap = prompt('Hi there, welcome to shop admin panel, what do you want(C,R,U,D)');
---
+++ Session 3/Video/index.js
@@ -0,0 +1,60 @@
+//////////////////// console.log('Init Ex');/////////////////////////
+// let movies = ['Naruto ', ' One Piece ', ' Bleach', 'OnePunch Man'];
+// console.log(movies);
+
+////////////////////// console.log('Create Ex');//////////////////
+// let newMovie = prompt('Enter your movies');
+// movies.push(newMovie);
+// console.log(movies);
+
+/////////////////////////console.log('Read EX')//////////////////
+// let i = prompt('Chose your movie');
+
+// if (i < 4) {
+//     console.log(movies[i])
+// }
+// else (null)
+
+////////////////////////console.log('Update Ex')/////////////////
+// let movieTitle = prompt('Change the name moive')
+// movies[0] = (movieTitle);
+// console.log(movies);
+
+// let movieTitle = prompt('Change the name moive')
+// let i = prompt('Chose your index')
+// if (i < 3) {
+//     console.log(movies)
+// }
+// else (null)
+// movies[i] = (movieTitle);
+// console.log(movies);
+
+////////////////////console.log('Delete Ex')/////////////
+// let i = prompt('Chose your position movie you want to delete')
+// movies.splice(i, 1);
+// console.log(movies);
+
+// let i = prompt('Chose your position movie you want to delete')
+// let n = prompt('How mant item you want to delete');
+// movies.splice(i, n);
+// console.log(movies);\
+
+console.log('Init Ex');
+let movies = ['Naruto ','One Piece ','Bleach', 'OnePunch Man','Yugi-Oh','Boruto'];
+console.log(movies);
+
+// // let i = 0;
+// // console.log(movies[i]);
+// // let n = movies.length;
+// // console.log(n);
+
+// for (let i = 0; i < movies.length; i++) {
+//     console.log(movies[i]);
+// }
+
+////////////////////////console.log('Read All Ex')////////////////
+console.log('1')
+for(let i = 0; i < movies.length; i++){
+    console.log(movies[i]);
+}
+console.log('')
